2f7814416bef423b965dda7c88fe26108759c2dc
kaushal <kaushal_chakka@srmap.edu.in>
2026-05-24 12:51:12 +0530

    2nd one

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import starsTexture from '../three.j/img/stars.jpg';
 import sunTexture from '../three.j/img/sun.jpg' ;
 import mercuryTexture from '../three.j/img/mercury.jpg';
+import venusTexture from '../three.j/img/venus.jpg';
+import earthTexture from '../three.j/img/earth.jpg';
+import marsTexture from '../three.j/img/mars.jpg';
 
 const renderer= new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
 const scene= new THREE.Scene();
@@ -38,7 +42,6 @@ scene.background = cubeTextureLoader.load([
 ]);
 
 const textureLoader= new THREE.TextureLoader();
-
 const sungeo=new THREE.SphereGeometry(16,30,30);
 const sunMat= new THREE.MeshBasicMaterial({
     map: textureLoader.load(sunTexture)
@@ -46,17 +49,44 @@ const sunMat= new THREE.MeshBasicMaterial({
 const sun=new THREE.Mesh(sungeo,sunMat);
 scene.add(sun);
 
-const mercurygeo=new THREE.SphereGeometry(4,30,30);
-const mercuryMat=new THREE.MeshBasicMaterial({
+const mercurygeo=new THREE.SphereGeometry(3.2,30,30);
+const mercuryMat=new THREE.MeshStandardMaterial({
     map:textureLoader.load(mercuryTexture)
 });
 const mercury=new THREE.Mesh(mercurygeo,mercuryMat);
-sun.add(mercury);
+const mercuryobj=new THREE.Object3D();
+mercuryobj.add(mercury);
+scene.add(mercuryobj);
+//sun.add(mercury);
 mercury.position.x=28;
+mercuryobj.addEventListener('click',()=>{
+    console.log('mercury');
+})
+const venusgeo=new THREE.SphereGeometry(5.8,30,30);
+const venusMat=new THREE.MeshStandardMaterial({
+    map:textureLoader.load(venusTexture)
+});
+const venus= new THREE.Mesh(venusgeo,venusMat);
+sun.add(venus);
+venus.position.x=42;
+
+const earthgeo=new THREE.SphereGeometry(6,30,30);
+const earthMat=new THREE.MeshStandardMaterial({
+    map:textureLoader.load(earthTexture)
+});
+const earth= new THREE.Mesh(earthgeo,earthMat);
+sun.add(earth);
+earth.position.x=60;
+
+const pointLight = new THREE.PointLight(0xFFFFFF, 1000, 300);
+pointLight.position.set(25, 0, 0);
+scene.add(pointLight);
 
 function animate(){
     sun.rotateY(0.004);
-    mercury.rotateY(0.004);
+    mercuryobj.rotateY(0.004);
+    venus.rotateY(0.004);
+    earth.rotateY(0.004);
     orbit.update();
     renderer.render(scene,camera);
 }
